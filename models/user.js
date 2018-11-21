e74156d63cbf2f1548ca06ceb15e866259661728
John Cen <john@desknibbles.com>
2018-11-21 16:56:34 -0500

    fixes the user schema model.

--- models/user.js
+++ models/user.js
@@ -4,7 +4,13 @@ const bcrypt = require('bcrypt');
 const userSchema = mongoose.Schema({
     _id : mongoose.Schema.Types.ObjectId,
     email :{type : String, required : true},
-    address :{type : String, required : true}
+    name: {
+      first: { type: String, required: true },
+      last: { type: String, required: true },
+      other: { type: String, required: false }
+    },
+    createdAt: { type: Date, require: true },
+    type: { type: StorageManager, required: true }, //one of: customer, owner, agent
     password: {
       type: String,
       required: true,
