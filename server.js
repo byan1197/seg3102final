ca5826071d7726aabce5e9cc188dbd10ede34c53
praiyon kumarasamy <praiyonkumarasamy@praiyons-MacBook-Pro.local>
2018-11-21 14:13:46 -0500

    added mongoose

--- server.js
+++ server.js
@@ -3,24 +3,18 @@ var http = require('http')
 var bodyParser = require('body-parser');
 var env = process.env.NODE_ENV || "development";
 var config = require('./config/config.json')[env];
-
+const mongoose = require('mongoose');
 // ROUTES
 var authAPI = require('./api/auth');
 // var userAPI = require('./api/user');
 // var propertyAPI = require('./api/property');
 
-// FIREBASE STUFF
-var globalConf = require('./config/config.json')["global"];
-var firebase = require('firebase');
-var admin = require('firebase-admin')
-var serviceAccount = require('./config/seg3102final-firebase-adminsdk-uaey7-1fec4eba6b.json');
+// MONGO
+mongoose.connect('mongodb://127.0.0.1:27017/seg', {
+    useNewUrlParser : true
+})
 
-var firebaseAdmin = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://seg3102final.firebaseio.com"
-});
-firebase.initializeApp(globalConf.firebase);
-// END OF FIREBASE STUFF
+// END MONGO
 
 app = express();
 
