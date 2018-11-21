ca02148754728f840c08026bf4e8a2ca3723950c
byan1197 <Bondyan97@gmail.com>
2018-11-21 15:29:41 -0500

    finished up models... for now

--- models/property.js
+++ models/property.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const propertySchema = mongoose.Schema({
-    _id : mongoose.Schema.Types.ObjectId,
-    email :{type : String, required : true}
-    
-    
+    _id: mongoose.Schema.Types.ObjectId,
+    email: { type: String, required: true },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    address: { type: String, required: true },
+    isAvailable: { type: boolean, required: true },
+    createdAt: { type: Date, require: true },
+    images: [{type: String, required: true}]
 });
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Property', propertySchema);
--- models/user.js
+++ models/user.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    _id : mongoose.Schema.Types.ObjectId,
-    email :{type : String, required : true},
-    address :{type : String, required : true}
+    _id: mongoose.Schema.Types.ObjectId,
+    email: { type: String, required: true },
+    name: {
+        first: { type: String, required: true },
+        last: { type: String, required: true },
+        other: { type: String, required: true }
+    },
+    createdAt: { type: Date, require: true },
+    type: {type: StorageManager, required: true} //one of: customer, owner, agent
 });
 
 module.exports = mongoose.model('User', userSchema);
