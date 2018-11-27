d40de789f2bde690ef929e0838871f16cded9584
John Cen <john.cen397@gmail.com>
2018-11-27 15:07:52 -0500

    Introduces Navbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography'
-// import AppBar from '@material-ui/core/AppBar';
-// import Button from '@material-ui/core/Button';
-// import {styled} from '@material-ui/core/styles';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import IconButton from '@material-ui/core/IconButton';
-// import Typography from '@material-ui/core/Typography';
-// import InputBase from '@material-ui/core/InputBase';
-// import Badge from '@material-ui/core/Badge';
-// import MenuItem from '@material-ui/core/MenuItem';
-// import Menu from '@material-ui/core/Menu';
-// import { fade } from '@material-ui/core/styles/colorManipulator';
-// import { withStyles } from '@material-ui/core/styles';
-// import MenuIcon from '@material-ui/icons/Menu';
-// import SearchIcon from '@material-ui/icons/Search';
-// import AccountCircle from '@material-ui/icons/AccountCircle';
-// import MailIcon from '@material-ui/icons/Mail';
-// import NotificationsIcon from '@material-ui/icons/Notifications';
-// import MoreIcon from '@material-ui/icons/MoreVert';
 import './App.css';
+import NavBar from './components/NavBar'
+import Typography from '@material-ui/core/Typography';
+
+// const Styled = createStyled({
+//   navBarTitle: {
+//     color: 'white',
+//     paddingTop: '0.1em'
+//   }
+// });
+// const primary = amber[50];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <Typography variant="h4" >OPR</Typography>
-        </div>
+        {/*<div Col={3} className="App-header">
+          <Typography variant="h2" color="secondary">OPR</Typography>
+          <Styled>
+            { ({classes}) => <Typography className={classes.navBarTitle} variant="h3">OPR</Typography>}
+          </Styled>
+        </div>*/}
+        <NavBar />
         <div className="Main">
           <Typography variant="h1">Hello</Typography>
         </div>
