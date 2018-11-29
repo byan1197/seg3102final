d4f7d9a4da5aa7fbd61123325f5ec1643da30216
John Cen <john.cen397@gmail.com>
2018-11-29 11:43:24 -0500

    Updates and adds some css

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 const Home = () => (
-  <div>
+  <div style={{width: '100%'}}>
     <p>this is the home page</p>
     <Link to='/test'>Go to Test page</Link>
   </div>
